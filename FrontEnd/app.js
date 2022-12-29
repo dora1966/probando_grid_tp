8acaaaa6aa1436b5d52d0732ca6d023720c9fcd5
Dora Abuaf <abuafdora@gmail.com>
2022-12-29 13:46:22 -0300

    intento grid v1

---
+++ FrontEnd/app.js
@@ -0,0 +1,39 @@
+const $container = document.getElementById("container");
+
+const URL_API = "https://rickandmortyapi.com/api/character";
+
+console.log(URL_API);
+
+fetch(URL_API)
+    .then((response) => response.json())
+    .then((data) => {
+        const characters = data.results;
+        console.log(characters)
+
+        for (let character of characters) {
+            console.log(character);
+            $container.innerHTML += `
+      <div class="option-name"> 
+        <img class="tarjeta" src="${character.image}" alt="imagen de ${character.name}"> 
+        <div class="info">
+        <br>
+        <br>
+        <br>
+        <br>
+        <h1 class="nombre"> ${character.name} </h1>
+        <br>
+        <br>
+        <h5 class="info-add"> Sex: ${character.gender} </h5>
+        <br>
+        <h5 class="info-add"> Live in: ${character.location.name} </h5>
+        <br> 
+        <h5 class="info-add"> Status: ${character.status}</h5>
+        <br>
+        <br>
+        <br>
+        </div>
+        </div>
+       `;
+        }
+    });
+
